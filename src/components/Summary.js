8f0d71300e3eac97eea275b3236a85f101d01a08
Joshua Chin <joshuachin191@gmail.com>
2020-10-20 10:06:09 +0800

    Added 'skills' field

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,14 +5,13 @@ class Summary extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            summary:"",
-            skills:""
+            summary:""
         }
         
     }
 
-    handleInput = (e, field) => {
-        this.setState({[field]: e.target.value});
+    handleInput = (e) => {
+        this.setState({summary: e.target.value});
     }
 
     render(){
@@ -27,7 +26,7 @@ class Summary extends React.Component {
                         if (editMode) {
                             element = <SummaryForm summaryForm={this.state.summary} handleInput={this.handleInput}/>
                         } else {
-                            element = <SummaryText summary={this.state.summary} skills={this.state.skills}/>
+                            element = <SummaryText summary={this.state.summary} />
                         }
                         return (
                             <React.Fragment>
@@ -47,7 +46,6 @@ const SummaryText = (props) => {
     return (
         <div>
             <p><b>Summary: </b>{props.summary}</p>
-            <p><b>Skills: </b>{props.skills}</p>
         </div>
     )
 }
@@ -57,18 +55,10 @@ const SummaryForm = (props) => {
         <form>
             <label><b>Summary: </b>
                 <textarea 
-                    placeholder='Write a summary of yourself here'
+                    placeholder='Write a summary of your skills and experiences here'
                     id='summary'
                     value={props.summary}
-                    onChange={(e) => props.handleInput(e, "summary")}
-                />
-            </label>
-            <label><b>Skills: </b>
-                <textarea 
-                    placeholder='Write a summary of your skills and experiences here'
-                    id='skills'
-                    value={props.skills}
-                    onChange={(e) => props.handleInput(e, "skills")}
+                    onChange={props.handleInput}
                 />
             </label>
         </form>
